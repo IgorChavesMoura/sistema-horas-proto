66e24e12695595315bfdf4df7a0c05dd282b3686
Igor Moura <igor310597@gmail.com>
2020-01-19 00:21:04 -0300

    Updates

---
+++ precache-manifest.556df502bcafaed7dbfd1d49ba83444b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89a6ddfd0a94c24f21f51d0e706e930",
+    "url": "/sistema-horas-proto/index.html"
+  },
+  {
+    "revision": "22f257f8605e06d234ac",
+    "url": "/sistema-horas-proto/static/css/main.ce775327.chunk.css"
+  },
+  {
+    "revision": "d61ba219ff8cbc458687",
+    "url": "/sistema-horas-proto/static/js/2.8551b5a1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/sistema-horas-proto/static/js/2.8551b5a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22f257f8605e06d234ac",
+    "url": "/sistema-horas-proto/static/js/main.84488a30.chunk.js"
+  },
+  {
+    "revision": "d12e8c46c13fc7cf562b",
+    "url": "/sistema-horas-proto/static/js/runtime-main.160f0da7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sistema-horas-proto/static/media/logo.5d5d9eef.svg"
+  }
+]);
